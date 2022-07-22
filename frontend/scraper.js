33d46f58e7194e5869b1cf8cd9101b4a92df4740
Addie Jackson <addieljackson3@gmail.com>
2022-07-21 21:11:57 -0600

    init of scraper script

---
+++ frontend/scraper.js
@@ -0,0 +1,27 @@
+albumList = [];
+
+// Find album blocks
+// album block: [data-slide-position-display="###"]
+//  title: .c-gallery-vertical-album__title, split by , or '
+//  artist: title split by the last comma or first '
+//  year: .c-gallery-vertical-album__subtitle, split by ,
+//  label: year split by ,
+//  description: .c-gallery-vertical-album__description
+//  artURL: src of .c-gallery-vertical-album__image
+
+// Model Data:
+// {"rank": 500,
+// "title": "Funeral",
+// "artist": "Arcade Fire",
+// "year": 2004,
+// "label": "Merge",
+// "description": "slkfjsdlfjs",
+// "artUrl":"https://sdfsdfsfds"
+// }
+
+// Loop through albums and identify elements
+
+// Add data to array
+albumList.push();
+
+// Load next 50 albums (click "Load More" button)
